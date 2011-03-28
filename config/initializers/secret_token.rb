f1c73aa3acac49bec77a0fb6b1d12f3eb3e0bec2
Andrew Djoga <andrew.djoga@gmail.com>
2011-03-29 00:16:34 +0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Currencies::Application.config.secret_token = '6962313ed45cdcf1a2683518dd00bcdf4e5058e8915d39b2d5b15bb3aae587ea25cd901b1eb5cbca08fbd596b5496d35a34c6b830a4cedb26e3c5656d8375241'
