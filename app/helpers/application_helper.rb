1f0f0c03b08966ba2a460a3183e05531aa959cab
Andrew Djoga <andrew.djoga@gmail.com>
2011-03-31 01:39:37 +0300

    Navigation was added.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def currency_pages_class
+    if ['currencies', 'country_lists'].include? controller_name
+      'active'
+    end
+  end
+
+  def trip_pages_class
+    'active' if controller_name == 'trips'
+  end
 end
