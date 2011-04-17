6b8a5026250fab9387f31ab313c3448792c1b84f
Andrew Djoga <andrew.djoga@gmail.com>
2011-04-18 01:07:50 +0300

    Currency.grouped_by_name was implemented.

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -7,4 +7,14 @@ class Currency < ActiveRecord::Base
 
   validates :name, :presence => true
   validates :code, :presence => true, :uniqueness => true
+
+  # Returns grouped currencies with country counts
+  def self.grouped_by_name
+    select('name, SUM(c_count) AS country_count').
+    joins('INNER JOIN (SELECT currency_id, COUNT(*) AS c_count FROM countries GROUP BY 1) as cc ' +
+          'ON cc.currency_id = currencies.id').
+    group('name').
+    order('name')
+  end
 end
+
--- spec/models/currency_spec.rb
+++ spec/models/currency_spec.rb
@@ -10,4 +10,27 @@ describe Currency do
   it { should validate_presence_of(:code) }
 
   it { should have_db_index(:name) }
+
+  describe ".grouped_by_name" do
+    before do
+      @dollar1 = Factory :currency, :name => 'Dollar'
+      @dollar2 = Factory :currency, :name => 'Dollar'
+      @euro = Factory :currency, :name => 'Euro'
+
+      Factory :country, :currency => @dollar1
+      Factory :country, :currency => @dollar1
+      Factory :country, :currency => @dollar2
+      Factory :country, :currency => @euro
+    end
+
+    it "should return grouped currencies with country counts" do
+      dollar, euro = Currency.grouped_by_name
+
+      dollar.name.should == 'Dollar'
+      dollar.country_count.should == 3
+
+      euro.name.should == 'Euro'
+      euro.country_count.should == 1
+    end
+  end
 end
