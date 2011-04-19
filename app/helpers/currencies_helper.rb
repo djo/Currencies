87f59e1520a891cf47a4391ab876eb5761353e60
Andrew Djoga <andrew.djoga@gmail.com>
2011-04-20 02:49:59 +0300

    Links to countries page were added.

--- app/helpers/currencies_helper.rb
+++ app/helpers/currencies_helper.rb
@@ -6,4 +6,12 @@ module CurrenciesHelper
       render 'currencies/description/available_currencies'
     end
   end
+
+  def countries_link(currency_name, label)
+    if user_signed_in?
+      link_to label, country_path(currency_name)
+    else
+      label
+    end
+  end
 end
